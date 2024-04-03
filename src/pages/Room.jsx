764f9caa8d9ec45b05b5045ce68ac63d250a2c38
Zawadirugenyi <zawadirugenyi@gmail.com>
2024-04-03 14:27:24 +0300

    finishing up The room page

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Stack, Image } from '@chakra-ui/react';
+import { Box, Stack, Image, Text, Input, Button } from '@chakra-ui/react';
 import Kryptons from '../components/images/Kryptons.jpeg';
 import Bedroom from '../components/images/data_Looks_16.jpeg';
 import Sitting_room from '../components/images/istockphoto-1333060261-612x612.jpeg';
@@ -16,7 +16,6 @@ export const Room = () => (
       marginBottom="150px"
       marginTop="-120px"
     >
-      {/* Add marginLeft to the first Image */}
       <Image src={Kryptons} alt="Main Image" width="737px" height="466px" maxWidth="100%" marginLeft="30px" />
       <Stack justify="flex-start" align="flex-start" spacing="16px">
         <Stack
@@ -40,6 +39,219 @@ export const Room = () => (
         </Stack>
       </Stack>
     </Stack>
+    <Stack direction="row" justify="center" align="center" spacing="60px" marginBottom="180px">
+      <Stack
+        paddingX="27px"
+        paddingY="60px"
+        justify="flex-start"
+        align="flex-start"
+        spacing="10px"
+        overflow="hidden"
+        marginLeft="40px"
+        marginTop="-45px"
+        borderColor="#05559F"
+        borderStartWidth="1px"
+        borderEndWidth="1px"
+        borderTopWidth="1px"
+        borderBottomWidth="1px"
+        background="#FFFFFF"
+        marginBottom="-45px"
+        minHeight="500px" 
+      >
+        <Stack justify="flex-start" align="flex-start" spacing="33px">
+          <Stack justify="flex-start" align="flex-start" spacing="42px">
+            <Text
+              fontFamily="Raleway"
+              fontWeight="regular"
+              fontSize="24px"
+              color="#05559F"
+             
+            >
+              Descriptions
+            </Text>
+            <Text
+              fontFamily="Raleway"
+              fontWeight="light"
+              fontSize="16px"
+              color="#000000"
+              width="590px"
+              height="113px"
+              maxWidth="100%"
+            >
+              we offer WiFi, bed, and water and we are located around Daystar Main
+              Campuswe offer WiFi, bed, and water and we are located around
+              Daystar Main Campus we offer WiFi, bed, and water and we are located
+              around Daystar Main Campuswe offer WiFi, bed, and water and we are
+              located around Daystar Main Campus
+            </Text>
+          </Stack>
+          <Stack justify="flex-start" align="flex-start" spacing="36px" marginBottom="-45px">
+            <Stack justify="flex-start" align="flex-start" spacing="65px">
+              <Text
+                fontFamily="Raleway"
+                fontWeight="regular"
+                fontSize="18px"
+                color="#05559F"
+              >
+                We offer:
+              </Text>
+              <Stack
+                direction="row"
+                justify="flex-start"
+                align="flex-start"
+                spacing="40px"
+              >
+                <Text
+                 fontFamily="Raleway"
+                 fontWeight="light"
+                 fontSize="16px"
+                 color="#000000"
+                >
+                 WiFi
+                </Text>
+                <Text
+                 fontFamily="Raleway"
+                 fontWeight="light"
+                 fontSize="16px"
+                 color="#000000"
+                >
+                 Worldrob
+                </Text>
+                <Text
+                 fontFamily="Ralway"
+                 fontWeight="light"
+                 fontSize="16px"
+                 color="#000000"
+                >
+                 Table
+                </Text>
+                <Text
+                 fontFamily="Raleway"
+                 fontWeight="light"
+                 fontSize="16px"
+                 color="#000000"
+                >
+                 Chair
+                </Text>
+                <Text
+                 fontFamily="Raleway"
+                 fontWeight="light"
+                 fontSize="16px"
+                 color="#000000"
+                >
+                 SwimmingPool
+                </Text>
+                <Text
+                 fontFamily="Raleway"
+                 fontWeight="light"
+                 fontSize="16px"
+                 color="#000000"
+                >
+                 Bed
+                </Text>
+              </Stack>
+            </Stack>
+            <Stack
+              paddingX="20px"
+              paddingY="13px"
+              direction="row"
+              justify="flex-start"
+              align="flex-start"
+              spacing="350px"
+              background="#ffffff"
+            >
+              <Text
+                fontFamily="Raleway"
+                fontWeight="light"
+                fontSize="16px"
+                color="#05559F"
+                marginLeft="-23px"
+              >
+                Price:
+              </Text>
+              <Text
+                fontFamily="Raleway"
+                fontWeight="light"
+                fontSize="16px"
+                color="#05559F"
+                 marginLeft="20px"
+              >
+                Ksh 15,000
+              </Text>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+      <Stack
+        paddingX="27px"
+        paddingY="60px"
+        justify="flex-start"
+        align="flex-start"
+        spacing="10px"
+        overflow="hidden"
+        marginLeft="40px"
+        marginTop="-45px"
+        borderColor="#05559F"
+        borderStartWidth="1px"
+        borderEndWidth="1px"
+        borderTopWidth="1px"
+        borderBottomWidth="1px"
+        background="#FFFFFF"
+        marginBottom="-45px"
+        minHeight="500px" 
+      >
+        <Stack justify="flex-start" align="flex-start" spacing="48px">
+          <Stack justify="flex-start" align="center" spacing="57px">
+            <Text
+              fontFamily="Raleway"
+              fontWeight="regular"
+              fontSize="24px"
+              color="#05559F"
+            >
+              Payment Method
+            </Text>
+            <Stack justify="flex-start" align="flex-start" spacing="30px">
+              <Input
+                placeholder="Phone Number"
+                size="xs"
+                width="360px"
+                height="43px"
+                fontFamily="Raleway"
+                maxWidth="100%"
+              />
+              <Input
+                placeholder="ksh 15,000"
+                size="xs"
+                width="360px"
+                height="43px"
+                maxWidth="100%"
+                fontFamily="Raleway"
+              />
+              <Input
+                placeholder="PIN"
+                size="xs"
+                width="360px"
+                height="43px"
+                maxWidth="100%"
+                fontFamily="Raleway"
+              />
+            </Stack>
+          </Stack>
+          <Button
+            size="lg"
+            colorScheme="blue"
+            width="360px"
+            height="48px"
+            maxWidth="100%"
+            fontFamily="Raleway"
+          >
+            Confirm Booking
+          </Button>
+        </Stack>
+      </Stack>
+    </Stack>
+
+    
  </Box>
 );
 
